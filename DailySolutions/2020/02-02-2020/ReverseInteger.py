2ddc52d4061338d107c781de204e4837ac3932b4
Noah Kaplan <nkaplan1@uncc.edu>
2020-02-05 19:09:44 -0500

    cleaned up solution for 02-02-2020

--- DailySolutions/2020/02-02-2020/ReverseInteger.py
+++ DailySolutions/2020/02-02-2020/ReverseInteger.py
@@ -1,18 +1,14 @@
 def reverse(num):
     preserveNegative = -1 if num < 0 else 1 
     num *= preserveNegative #make number positive
-    results = []
+    
+    finalResult = 0
     while num > 0:
         lastDigit = num%10
-        results.append(lastDigit)
+        finalResult *= 10
+        finalResult += lastDigit
         num = num - lastDigit
         num /= 10
-
-    finalResult = 0
-    count = len(results) - 1 
-    for digit in results:
-        finalResult += (digit * pow(10, count))
-        count -= 1
 
     finalResult *= preserveNegative
     return finalResult

584b45dc0bcc246d8a9aae9a74ead2734e461c6b
Noah Kaplan <nkaplan1@uncc.edu>
2020-06-11 10:28:43 -0400

    added apis to retrieve question and solution text

--- WebApi/QuestionAPI.py
+++ WebApi/QuestionAPI.py
@@ -4,7 +4,17 @@ import json
 
 question_api = Blueprint("question_api", __name__)
 
-@question_api.route("/questions")
+@question_api.route("/questions/")
 def questionList():
     result = QuestionService.getQuestionList()
     return jsonify({"questions": result})
+
+@question_api.route("/questions/<string:questionDate>/")
+def getQuestionByDate(questionDate):
+    result = QuestionService.getQuestionByDate(questionDate)
+    return jsonify({"question": result})
+
+@question_api.route("/questions/<string:questionDate>/solution/")
+def getSolutionByDate(questionDate):
+    result = QuestionService.getSolutionByDate(questionDate)
+    return jsonify({"solution": result})
--- WebApi/QuestionService.py
+++ WebApi/QuestionService.py
@@ -1,3 +1,39 @@
+import os
+import re
+
 class QuestionService():
     def getQuestionList():
-        return {"date": "01/01/2020", "title": "default question"}
+        os.chdir("DailySolutions")
+        years = os.listdir(os.getcwd())
+        dates = []
+        for year in years:
+            os.chdir(year)
+            directories = os.listdir(os.getcwd())
+            for directory in directories:
+                dates.append(directory)
+            os.chdir("..")
+        os.chdir("..")
+        return dates
+
+    def getQuestionByDate(date):
+        parsedDate = date.split("-")
+        os.chdir("DailySolutions/"+parsedDate[2]+"/"+date)
+        questionFile = open("question.txt", "r")
+        question = questionFile.read()
+        questionFile.close()
+        os.chdir("../../..")
+        return question
+
+    def getSolutionByDate(date):
+        parsedDate = date.split("-")
+        solution = ""
+        os.chdir("DailySolutions/"+parsedDate[2]+"/"+date)
+        files = os.listdir(os.getcwd())
+        for file in files:
+            if re.match("^.*\.py", file) and not re.match("^.*Tests\.py", file) and not re.match("main.py", file):
+                solutionFile = open(file, "r")
+                solution = solutionFile.read() + "\n " + solution
+                solutionFile.close()
+        os.chdir("../../..")
+        return solution
+    
